6013bbd74f2d00d173511ec6c014042a9cf6ea47
Yushi_O <oguma292@yahoo.co.jp>
2022-12-10 18:31:25 +0900

    リクエストテスト生成部分の完成

--- request_test_generator.rb
+++ request_test_generator.rb
@@ -2,17 +2,16 @@ require 'active_support/inflector'
 require 'erb'
 
 class RequestTestGenerator
-  def initialize(controller_name)
+  def initialize(controller_name, excluded_actions: [])
     @controller_name = controller_name.underscore.singularize
+    @actions = %w[index show new edit create update destroy] - excluded_actions
   end
 
   def generate
-    path_output = File.join(__dir__, 'out', "#{@controller_name.pluralize}_spec.rb")
-    path_template = File.join(__dir__, 'lib', 'request_spec.rb.erb')
+    path_output = "#{__dir__}/out/#{@controller_name.pluralize}_spec.rb"
+    path_template = "#{__dir__}/lib/request_spec.rb.erb"
 
-    request_test_erb = ERB.new(File.read(path_template))
+    request_test_erb = ERB.new(File.read(path_template), trim_mode: 2)
     File.write(path_output, request_test_erb.result(binding))
   end
 end
-
-RequestTestGenerator.new('test').generate if __FILE__ == $PROGRAM_NAME
