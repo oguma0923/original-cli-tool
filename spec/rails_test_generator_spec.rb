3e7a3aceee08c4f8cc83127da1352c6aec5951cc
Yushi_O <oguma292@yahoo.co.jp>
2022-12-17 02:42:19 +0900

    RequestTestGeneratorのテストを作成

---
+++ spec/rails_test_generator_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../request_test_generator'
+
+RSpec.describe RequestTestGenerator do
+  let(:controller_name) { 'test' }
+  let(:generated_file) { 'out/tests_spec.rb' }
+
+  describe 'generate' do
+    after { File.delete(generated_file) }
+
+    context 'controller_name' do
+      it 'controller_nameに対応したテストファイルが生成されること' do
+        RequestTestGenerator.new(controller_name, []).generate
+        expect(File).to exist(generated_file)
+      end
+    end
+
+    context 'excluded_actions' do
+      it '指定したアクションに関するテストを除外できること' do
+        RequestTestGenerator.new(controller_name, ['index']).generate
+        expect(File.read(generated_file)).not_to include("describe 'GET /tests' do")
+      end
+
+      it 'アクションを指定しなければすべてのアクションに関するテストが生成されること' do
+        RequestTestGenerator.new(controller_name, []).generate
+        expect(File.read(generated_file)).to include("describe 'GET /tests' do")
+      end
+    end
+  end
+end
