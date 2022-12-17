48bc0a97ee0bfa7c24cfa1f9226bdab7dcd29462
Yushi_O <oguma292@yahoo.co.jp>
2022-12-17 18:54:53 +0900

    rails_test_generatorのテストを作成

---
+++ spec/rails_test_generator_spec.rb
@@ -0,0 +1,41 @@
+require_relative '../rails_test_generator'
+
+RSpec.describe RailsTestGenerator do
+  let(:controller_name) { 'test' }
+  let(:request_test_file) { 'out/tests_spec.rb' }
+
+  describe 'generate --request' do
+    after { File.delete(request_test_file) }
+
+    context '除外するアクションを指定しないとき' do
+      it 'controller_nameに対応したリクエストテストが生成される' do
+        RailsTestGenerator.new(['--request', controller_name]).generate
+        expect(File).to exist(request_test_file)
+      end
+
+      it 'すべてのアクションに関するテストが生成されること' do
+        RailsTestGenerator.new(['--request', controller_name]).generate
+        expect(File.read(request_test_file)).to include("describe 'GET /tests' do")
+        expect(File.read(request_test_file)).to include("describe 'GET /tests/:id' do")
+        expect(File.read(request_test_file)).to include("describe 'GET /tests/new' do")
+        expect(File.read(request_test_file)).to include("describe 'GET /tests/:id/edit' do")
+        expect(File.read(request_test_file)).to include("describe 'POST /tests' do")
+        expect(File.read(request_test_file)).to include("describe 'PATCH/PUT /tests/:id' do")
+        expect(File.read(request_test_file)).to include("describe 'DELETE /tests/:id' do")
+      end
+    end
+
+    context '除外するアクションを指定したとき' do
+      it 'controller_nameに対応したリクエストテストが生成される' do
+        RailsTestGenerator.new(['--request', controller_name, '--exclude', 'index,show']).generate
+        expect(File).to exist(request_test_file)
+      end
+
+      it '指定したアクションが除外されること' do
+        RailsTestGenerator.new(['--request', controller_name, '--exclude', 'index,show']).generate
+        expect(File.read(request_test_file)).not_to include("describe 'GET /tests' do")
+        expect(File.read(request_test_file)).not_to include("describe 'GET /tests/:id' do")
+      end
+    end
+  end
+end
